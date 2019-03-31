9de8f7480d69aa3b9266d7d738b3aa0a401ad3cd
Jwalanta Shrestha <jwalanta@gmail.com>
2019-03-31 10:05:57 -0700

    fix weather polling

--- weather.py
+++ weather.py
@@ -19,10 +19,15 @@ class Weather:
 		if os.path.isfile(self.forecast_file):
 			# check if file has been modified
 			if os.path.getmtime(self.forecast_file) != self.forecast_mtime:
-				self.forecast_mtime = os.path.getmtime(self.forecast_file)
 				f = open(self.forecast_file, "r")
-				self.forecast = json.load(f)
+				forecast = json.load(f)
 				f.close()
+
+				# error will be raised by now if the file is empty
+				# check if the required forecast keys are there
+				if "currentobservation" in forecast and "data" in forecast:
+					self.forecast = forecast
+					self.forecast_mtime = os.path.getmtime(self.forecast_file)
 		else:
 			# file can be unavailable if the cron is writing on it
 			# reset only if it's older than an hour
@@ -33,14 +38,22 @@ class Weather:
 	def get_current_temperature(self):
 		try:
 			self.update()
+		except Exception as e:
+			pass
+
+		if "currentobservation" in self.forecast:
 			return str(self.forecast['currentobservation']['Temp']) + "F"
-		except:
+		else:
 			return ""
 
 	def get_today_forecast(self):
 		try:
 			self.update()
+		except Exception as e:
+			pass
+
+		if "data" in self.forecast:
 			t = self.forecast["data"]["temperature"]
 			return (max(t[0],t[1]), min(t[0],t[1]))
-		except:
+		else:
 			return ("","")
